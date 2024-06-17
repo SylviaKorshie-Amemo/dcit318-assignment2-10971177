0beeb1f25a0611c65402c26f4d33287234ebff68
Sylvia Korshie-Amemo <sekorshie-amemo@st.ug.edu.gh>
2024-06-17 15:34:14 -0700

    Abstract Class and Methods changes

---
+++ Assignment/Abstract.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Assignment
+{
+    abstract class Shape
+    {
+
+        public abstract double GetArea();
+
+        class Circle : Shape
+        {
+
+            public double Radius { get; set; }
+
+            public override double GetArea();
+			{
+				return Math.PI* Math.Pow(Radius, 2);
+        }
+
+
+        class Rectangle : Shape
+        {
+            public double length { get; set; }
+
+            public double width { get; set; }
+
+            public override double GetArea()
+            {
+                return length * width;
+
+            }
+
+            static void main(string[] args)
+            {
+                Circle circle = new Circle { Radius = 5 };
+
+                Console.WriteLine($"Circle Area: {circle.GetArea():F2}");
+
+                Rectangle rectangle = new Rectangle { length = 4, width = 3 };
+
+                Console.WriteLine($"Rectangle Area: {rectangle.GetArea():F2}");
+            }
+
+        }
+
+    }
+
+}
