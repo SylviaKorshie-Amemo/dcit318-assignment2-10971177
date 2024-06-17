bc93a052f051a778cefe607c8d3a9ae01ef3cf14
Sylvia Korshie-Amemo <sekorshie-amemo@st.ug.edu.gh>
2024-06-17 03:46:19 -0700

    Inheritnace and method overriding

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// Inheritance and Method Overriding
+class Animal
+{
+
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+    static void Main(string[] args)
+    {
+        // Inheritance and Method Overriding
+        Animal animal = new Animal();
+        animal.MakeSound(); // Prints "Some generic sound"
+
+        Dog dog = new Dog();
+        dog.MakeSound(); // Prints "Bark"
+
+        Cat cat = new Cat();
+        cat.MakeSound(); // Prints "Meow"
+    }
+}
